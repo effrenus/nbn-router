a31ba1b20a8a65fcc728d688dc49622343152e92
effrenus <effrenus@gmail.com>
2015-08-20 17:36:11 +0300

    gulp task fixies, fix visual bugs

--- gulp/tasks/styles.js
+++ gulp/tasks/styles.js
@@ -3,6 +3,9 @@ import sass from 'gulp-sass';
 import plumber from 'gulp-plumber';
 import errorHandler from '../errorHandler';
 import browserSync from 'browser-sync';
+import autoprefixer from 'gulp-autoprefixer';
+import csso from 'gulp-csso';
+import gulpif from 'gulp-if';
 
 gulp.task('styles', () => {
 
@@ -10,6 +13,11 @@ gulp.task('styles', () => {
 		.src('app/styles/default.sass')
 		.pipe(plumber({errorHandler}))
 		.pipe(sass())
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+      cascade: false
+		}))
+		.pipe(gulpif('production' === process.env.NODE_ENV, csso()))
 		.pipe(gulp.dest('dist/styles'))
 		.pipe(browserSync.stream());
 
